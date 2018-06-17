b2d756fc43f318b5dad941145e956316c700979c
Wilfred Hughes <me@wilfred.me.uk>
2018-06-17 19:09:51 +0100

    Configure Travis

---
+++ test/test-helper.el
@@ -0,0 +1,20 @@
+;;; test-helper.el --- Helper for tests              -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018 Wilfred Hughes
+
+;; Author:  <me@wilfred.me.uk>
+
+;;; Code:
+
+(require 'ert)
+(require 'f)
+
+(let ((deadgrep-dir (f-parent (f-dirname (f-this-file)))))
+  (add-to-list 'load-path deadgrep-dir))
+
+(require 'undercover)
+(undercover "deadgrep.el"
+	    (:exclude "*-test.el")
+	    (:report-file "/tmp/undercover-report.json"))
+
+;;; test-helper.el ends here
