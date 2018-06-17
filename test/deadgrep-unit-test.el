6cde5e09b952ebe7cc0f5e145c7b15bf2f6816b4
Wilfred Hughes <me@wilfred.me.uk>
2018-06-17 23:48:46 +0100

    Smoke test and movement tests

--- test/deadgrep-unit-test.el
+++ test/deadgrep-unit-test.el
@@ -18,3 +18,47 @@
      (not
       (eq (get-text-property 1 'face result)
           'font-lock-constant-face)))))
+
+(ert-deftest deadgrep-smoke-test ()
+  (cl-letf (((symbol-function 'read-from-minibuffer)
+             (lambda (&rest _args) "foo")))
+    (deadgrep)))
+
+(ert-deftest deadgrep-forward ()
+  (cl-letf (((symbol-function 'read-from-minibuffer)
+             (lambda (&rest _args) "a-unique-string")))
+    (deadgrep)
+
+    ;; Smoke test.
+    (deadgrep-forward)
+
+    ;; Moving forward, when point is already on the last item, should
+    ;; not error.
+    (goto-char (point-max))
+    (deadgrep-forward)
+
+    ;; We should end up with point on an item.
+    (goto-char (point-min))
+    (deadgrep-forward)
+    (should
+     (deadgrep--item-p (point)))))
+
+(ert-deftest deadgrep-backward ()
+  (cl-letf (((symbol-function 'read-from-minibuffer)
+             (lambda (&rest _args) "a-unique-string")))
+    (deadgrep)
+
+    ;; Smoke test.
+    (goto-char (point-max))
+    (deadgrep-backward)
+
+    ;; Moving backward, when point is already on the first item, should
+    ;; not error.
+    (goto-char (point-min))
+    (deadgrep-backward)
+
+    ;; We should end up with point on an item.
+    (goto-char (point-max))
+    (deadgrep-backward)
+    (should
+     (deadgrep--item-p (point)))))
