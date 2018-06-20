fbb58d3c1399fe94af82793c507a10213b36bc85
Wilfred Hughes <me@wilfred.me.uk>
2018-06-21 00:27:41 +0100

    Add URL in header

--- deadgrep.el
+++ deadgrep.el
@@ -3,6 +3,7 @@
 ;; Copyright (C) 2018  Wilfred Hughes
 
 ;; Author: Wilfred Hughes <me@wilfred.me.uk>
+;; URL: https://github.com/Wilfred/deadgrep
 ;; Keywords: tools
 ;; Version: 0.1
 ;; Package-Requires: ((emacs "25.1") (dash "2.12.0") (s "1.11.0") (spinner "1.7.3") (projectile "0.14.0"))
