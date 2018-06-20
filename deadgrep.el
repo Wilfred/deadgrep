7e841ddfaf52f36521f0a990a6a8ea766e916d71
Wilfred Hughes <me@wilfred.me.uk>
2018-06-21 00:07:07 +0100

    Expand commentary to describe usage

--- deadgrep.el
+++ deadgrep.el
@@ -26,6 +26,8 @@
 ;; emacs. This is a bespoke mode that does not rely on
 ;; compilation-mode, but tries to be a perfect fit for ripgrep.
 
+;; Install from MELPA, then `M-x ripgrep' will do a search!
+
 ;;; Code:
 
 (require 'cl-lib)
