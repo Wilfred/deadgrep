d3c74707eeb0357f2449aec82b844fb8a48bd921
Wilfred Hughes <me@wilfred.me.uk>
2019-03-12 09:42:33 +0000

    Correcting example in header

--- deadgrep.el
+++ deadgrep.el
@@ -27,7 +27,7 @@
 ;; Emacs.  This is a bespoke mode that does not rely on
 ;; compilation-mode, but tries to be a perfect fit for ripgrep.
 
-;; Install from MELPA, then `M-x ripgrep' will do a search!
+;; Install from MELPA, then `M-x deadgrep' will do a search!
 
 ;;; Code:
 
