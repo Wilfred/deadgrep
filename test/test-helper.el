061216a0aca7693b1e90a73ccb7a1cbd455ab200
Wilfred Hughes <me@wilfred.me.uk>
2021-01-30 23:06:13 -0800

    Prefer coveralls over codecov
    It's more extensively documented in the undercover docs, and deadgrep
    has some historical data there already.

--- test/test-helper.el
+++ test/test-helper.el
@@ -13,9 +13,6 @@
   (add-to-list 'load-path deadgrep-dir))
 
 (require 'undercover)
-(undercover "deadgrep.el"
-	    (:exclude "*-test.el")
-            (:report-file "/tmp/undercover-report.json")
-            (:report-format 'codecov))
+(undercover "deadgrep.el" (:exclude "*-test.el"))
 
 ;;; test-helper.el ends here
