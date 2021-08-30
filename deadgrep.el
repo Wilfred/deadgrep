4ec21e644ef482a913c64f068ec8d602eedac1c6
Wilfred Hughes <me@wilfred.me.uk>
2021-08-29 23:56:36 -0700

    Prefer project.el functions to find the root of a project
    Fixes #50

--- deadgrep.el
+++ deadgrep.el
@@ -1436,7 +1436,8 @@ Otherwise, return PATH as is."
   (let ((root default-directory)
         (project (project-current)))
     (when project
-      (setq root (cdr project)))
+      (when-let ((roots (project-roots project)))
+        (setq root (car roots))))
     (when root
       (deadgrep--lookup-override root))))
 
