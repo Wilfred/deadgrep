73445a2883e7bd9e3db0fd5cc469bafa467e7fd7
Michael Wolf <maw@pobox.com>
2022-07-03 10:13:10 -0500

    Add deadgrep-change-search-case and make it available with C

--- deadgrep.el
+++ deadgrep.el
@@ -931,6 +931,15 @@ Returns a list ordered by the most recently accessed."
    ((eq deadgrep--search-type 'words) (setq deadgrep--search-type 'regexp))
    ((eq deadgrep--search-type 'regexp) (setq deadgrep--search-type 'string)))
   (deadgrep-restart))
+
+(defun deadgrep-change-search-case ()
+  (interactive)
+  (cond
+   ((eq deadgrep--search-case 'smart) (setq deadgrep--search-case 'sensitive))
+   ((eq deadgrep--search-case 'sensitive) (setq deadgrep--search-case 'ignore))
+   ((eq deadgrep--search-case 'ignore) (setq deadgrep--search-case 'smart)))
+  (deadgrep-restart))
+
 (defvar deadgrep-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "RET") #'deadgrep-visit-result)
@@ -939,6 +948,7 @@ Returns a list ordered by the most recently accessed."
 
     (define-key map (kbd "S") #'deadgrep-search-term)
     (define-key map (kbd "T") #'deadgrep-change-search-type)
+    (define-key map (kbd "C") #'deadgrep-change-search-case)
     (define-key map (kbd "D") #'deadgrep-directory)
     (define-key map (kbd "^") #'deadgrep-parent-directory)
     (define-key map (kbd "g") #'deadgrep-restart)
